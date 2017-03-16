c7803dfb1a8f7682b66bd50720ef470a8be1d988
Steven-Braun <sbraun@hawaii.edu>
2017-03-15 20:18:41 -1000

    updated UI

--- src/Main.java
+++ src/Main.java
@@ -5,30 +5,57 @@ public class Main {
 	public static void main(String[] args){
 		EZ.initialize(800, 600);
 		EZ.addImage("tofrom.png", 400, 292);
-		EZImage plane = EZ.addImage("plane.png", 300, 300);
+		EZImage plane = EZ.addImage("plane_transparent.png", 300, 300);
 		EZCircle station = EZ.addCircle(400, 300, 50, 50, Color.black, false);
 		EZCircle dot = EZ.addCircle(400, 300, 7, 7, Color.black, true);
 		int clickX = EZInteraction.getXMouse();
 		int clickY = EZInteraction.getYMouse();	
+		EZText display = EZ.addText(400, 310, "", Color.RED);
+		EZText good = EZ.addText(20, 20, "", Color.BLACK);
 		EZLine line = EZ.addLine(400, 300, 300, 300, Color.RED);
+		
 		while(true){
 			clickX = EZInteraction.getXMouse();
 			clickY = EZInteraction.getYMouse();	
-			if(EZInteraction.wasMouseLeftButtonPressed()){
+			boolean signal = goodBadSignal(clickX, clickY);
+			if (signal == true)
+				{
+					display.show();
+					good.color = Color.BLACK;
+					good.msg = "GOOD!";
+				} else {
+					good.color = Color.red;
+					good.msg = "BAD!";
+					display.hide();
+				}
+			if(EZInteraction.wasMouseLeftButtonPressed() || EZInteraction.isMouseLeftButtonDown()){
+				//display.hide();
 				plane.translateTo(clickX, clickY);
 				line.setPoint2(clickX, clickY);
-				calculateAngle(clickX, clickY);
+				double angle = Math.floor(calculateAngle(clickX, clickY));
+				String displayangle = String.valueOf(angle);
+				display.msg = displayangle;
 			}
 			EZ.refreshScreen();
 		}
 	}
 	
-	static void calculateAngle(int x, int y){
+	static double calculateAngle(int x, int y){
 		double cos = x-400;
 		double sin = 300-y;
-		double angle = sin/cos;
-		Math.atan(angle);
-		System.out.println(angle);
-		
+		double angle = (Math.atan2(sin,cos)*360/3.14159265)/2;
+		if(angle < 0){
+			return 360+angle;
+		} else {
+			return angle;
+		}
+	}
+	
+	static boolean goodBadSignal(int x, int y){
+		if(x > 750 || y > 550){
+			return false;
+		} else {
+			return true;
+		}
 	}
 }
